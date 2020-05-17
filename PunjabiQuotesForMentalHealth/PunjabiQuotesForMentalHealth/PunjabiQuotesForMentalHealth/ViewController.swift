3566903e86c1564e3c6683ac2edf0f0425864870
vishalk21 <58450600+vishalk21@users.noreply.github.com>
2020-05-17 15:58:54 -0400

    New Changes
    I made few changes, but I got stuck. Also I wasn't sure if I should create a new branch or what ?

---
+++ PunjabiQuotesForMentalHealth/PunjabiQuotesForMentalHealth/PunjabiQuotesForMentalHealth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PunjabiQuotesForMentalHealth
+//
+//  Created by Vishal Kaushal on 5/14/20.
+//  Copyright © 2020 vishal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+  
+    struct jsonFile {
+             let text : String
+    
+    }
+    
+        let jsonQuotes = jsonFile(text: "Kidda ji")
+               print(jsonQuotes)
+    
+    }
+}
+
