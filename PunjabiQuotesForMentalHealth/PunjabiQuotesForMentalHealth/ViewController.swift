f9c6d3ff2c97797f8750714324f2216bba5d1f93
Krishna <alanka.krishnakumar@gmail.com>
2020-06-01 23:02:04 -0400

    Notes to read

--- PunjabiQuotesForMentalHealth/PunjabiQuotesForMentalHealth/ViewController.swift
+++ PunjabiQuotesForMentalHealth/PunjabiQuotesForMentalHealth/ViewController.swift
@@ -7,20 +7,33 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    var firstName : String?
+    let last: String = "krishna"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        
         // Do any additional setup after loading the view.
-        guard let path = Bundle.main.path(forResource: "quotes", ofType: "json") else {return}
+        guard let path = Bundle.main.path(forResource: "quotes", ofType: "json") else {
+            return
+        }
+        
+        //Convering to URl
         let url = URL(fileURLWithPath: path)
-        let data = try! Data(contentsOf: url)
+        
+        //get the data from the URL
+       // let data = try! Data(contentsOf: url)
         
         do{
             let data = try! Data(contentsOf: url)
-            let json = try! JSONSerialization.jsonObject(with: data, options:.mutableContainers)
-            print(json)
+            let json = try! JSONSerialization.jsonObject(with: data, options:.mutableLeaves)
+            print(jsonData)
         }catch{
             print(error)
         }
@@ -28,4 +41,12 @@ class ViewController: UIViewController {
 }
     
 }
+
+//Notes to read
+//Let, var
+//Options ?
+//fore wrapping and when we can apply that
+//Convert the data to Objects
+//Read for Codable and decodable
+//read the values from objects
 
